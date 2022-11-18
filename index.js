b402b5f25d7807d796e29e80592585194c2887fe
ymc9 <104139426+ymc9@users.noreply.github.com>
2022-11-18 20:25:10 +0800

    fix: dotenv file resolution logic
    - make sure to stop walking up in directories when we hit root
    - attach found .env path to pluginData
    - clean up logic for combining dot env and system environment variables

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
+const path = require("path");
 const fs = require("fs");
 
 const ENV = process.env.NODE_ENV || "development";
 
 function _findEnvFile(dir) {
-    if (!fs.existsSync(dir)) return false;
-    return fs.existsSync(`${dir}/.env.${ENV}`)
-        ? `${dir}/.env.${ENV}`
-        : fs.existsSync(`${dir}/.env`)
-            ? `${dir}/.env`
-            : _findEnvFile(path.resolve(dir, "../")); //undefined
+    if (!fs.existsSync(dir)) return undefined;
+
+    if (fs.existsSync(`${dir}/.env.${ENV}`)) {
+        return `${dir}/.env.${ENV}`;
+    } else if (fs.existsSync(`${dir}/.env`)) {
+        return `${dir}/.env`;
+    } else {
+        const next = path.resolve(dir, "../");
+        if (next === dir) {
+            // at root now, exit
+            return undefined;
+        } else {
+            return _findEnvFile(next);
+        }
+    }
 }
 
 module.exports = {
     name: "env",
 
     setup(build) {
-
         build.onResolve({ filter: /^env$/ }, async (args) => {
             return {
-                path: _findEnvFile(args.pluginData.currentDir),
-                //path: args.path,
+                path: args.path,
+                pluginData: {
+                    ...args.pluginData,
+                    envPath: _findEnvFile(args.resolveDir),
+                },
                 namespace: "env-ns",
             };
         });
 
         build.onLoad({ filter: /.*/, namespace: "env-ns" }, async (args) => {
-            //let envPath = _findEnvFile(args.pluginData.currentDir)
-            let envPath = args.path;
-            let contents = JSON.stringify(process.env)
+            let envPath = args.pluginData?.envPath;
+            let config = {};
 
             if (envPath) {
                 // read in .env file contents and combine with regular .env:
-                let data = await fs.promises.readFile(envPath, "utf8")
-                const buf = Buffer.from(data)
-                const config = require("dotenv").parse(buf)
-                contents = JSON.stringify({ ...process.env, ...config })
+                let data = await fs.promises.readFile(envPath, "utf8");
+                const buf = Buffer.from(data);
+                config = require("dotenv").parse(buf);
             }
 
             return {
-                contents,
+                // let dotenv override system environment variables
+                contents: JSON.stringify({ ...process.env, ...config }),
                 loader: "json",
             };
         });
-    }
+    },
 };
